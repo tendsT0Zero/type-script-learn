d582499471926bcb725386900f2bf2eb587380cc
tendsT0Zero <nayeemofficial80@gmail.com>
2026-04-29 11:17:55 +0600

    Commonly used primitive datatype and their declaration in code

--- src/29-04-26/index.ts
+++ src/29-04-26/index.ts
@@ -3,3 +3,14 @@ function greet(name: string): string {
 }
 
 console.log(greet("Nayeem"));
+
+// primitive data types in typescript
+
+let examCode: string = "TS-101";
+let totalMarks: number = 100;
+let isPassed: boolean = true;
+let studentName: string = "Nayeem";
+let studentAge: number = 20;
+let studentGrade: string = "A+";
+
+let studentPin: number | string = 12345; // can be a number or a string
